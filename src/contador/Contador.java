f463e52968140c116b4ef0eb801e3f377e494dce
C-xiloj <ordchris2022@gmail.com>
2026-02-13 23:38:43 -0600

    V1.0 Analizador

---
+++ src/contador/Contador.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package contador;
+
+/**
+ *
+ * @author CompuFire
+ */
+public class Contador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_contador frm = new frm_contador();
+        frm.show();
+    }
+    
+}
